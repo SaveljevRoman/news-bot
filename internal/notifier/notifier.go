9b2ed464c4532b13f3e6d6d177410cff6f4e9b33
roman.saveljev <saveljev.roman@gmail.com>
2023-11-02 18:13:08 +0300

    notifier todo

---
+++ internal/notifier/notifier.go
@@ -0,0 +1,77 @@
+package notifier
+
+import (
+	"context"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"news-bot/internal/model"
+	"time"
+)
+
+type ArticleProvider interface {
+	AllNotPosted(ctx context.Context, since time.Time, limit uint64) ([]model.Article, error)
+	MarkPosted(ctx context.Context, id int64) error
+}
+
+type Summarizer interface {
+	Summarize(ctx context.Context, text string) (string, error)
+}
+
+type Notifier struct {
+	articles         ArticleProvider
+	summarizer       Summarizer
+	bot              *tgbotapi.BotAPI
+	sendInterval     time.Duration
+	lookupTimeWindow time.Duration
+	channelID        int64
+}
+
+func New(
+	articles ArticleProvider,
+	summarizer Summarizer,
+	bot *tgbotapi.BotAPI,
+	sendInterval time.Duration,
+	lookupTimeWindow time.Duration,
+	channelID int64,
+) *Notifier {
+	return &Notifier{
+		articles:         articles,
+		summarizer:       summarizer,
+		bot:              bot,
+		sendInterval:     sendInterval,
+		lookupTimeWindow: lookupTimeWindow,
+		channelID:        channelID,
+	}
+}
+
+func (n *Notifier) SelectAndSendArticle(ctx context.Context) error {
+	//time.Now().Add(-n.lookupTimeWindow) вычитание промежутка времени из текущего
+	topOneArticles, err := n.articles.AllNotPosted(ctx, time.Now().Add(-n.lookupTimeWindow), 1)
+	if err != nil {
+		return err
+	}
+
+	if len(topOneArticles) == 0 {
+		return nil
+	}
+
+	article := topOneArticles[0]
+
+	summary, err := n.extractSummary(ctx, article)
+	if err != nil {
+		return err
+	}
+
+	if err := n.SendArticle(article, summary); err != nil {
+		return err
+	}
+
+	return n.articles.MarkPosted(ctx, article.ID)
+}
+
+func (n *Notifier) extractSummary(ctx context.Context, article model.Article) (string, error) {
+	// todo
+}
+
+func (n *Notifier) SendArticle(article model.Article, summary string) error {
+	// todo
+}
